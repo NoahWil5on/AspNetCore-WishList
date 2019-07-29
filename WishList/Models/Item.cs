b15c7689f1f6d11ac1815e33b637f66cd719cd8f
Noah Wilson <noah.wilson@endurance.com>
2019-07-29 11:51:25 -0400

    module 4

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Description  { get; set; }
+    }
+}
